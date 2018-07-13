41525ce0271074baae4fbde01e1ca1bffde00a82
kimghoon777 <38814568+kimghoon777@users.noreply.github.com>
2018-07-13 10:52:44 +0900

    Update signal_slot_00_basic.py

--- QtFramework/QtWidgets/Signal_Slot/signal_slot_00_basic.py
+++ QtFramework/QtWidgets/Signal_Slot/signal_slot_00_basic.py
@@ -19,15 +19,14 @@ __author__ = "Deokyu Lim <hong18s@gmail.com>"
 class Form(QWidget):
     def __init__(self):
         QWidget.__init__(self, flags=Qt.Widget)
-        self.dl = QDial()
-        self.sd = QSlider(Qt.Horizontal)
         self.init_widget()
 
     def init_widget(self):
         self.setWindowTitle("Signal Slot")
         form_lbx = QBoxLayout(QBoxLayout.TopToBottom, parent=self)
         self.setLayout(form_lbx)
-
+        self.dl = QDial()
+        self.sd = QSlider(Qt.Horizontal)
         # 시그널 슬롯 연결
         # 다이얼의 값이 변하면 슬라이더의 값을 변경하는 슬롯과 연결
         # 슬라이더의 값이 변화하면 다이얼의 값을 변경하는 슬롯과 연결
